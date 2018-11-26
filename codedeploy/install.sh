0e79a6f2d3887c0aeca0b1ce90b9fd2d356dd7cc
thaleslnr <44848784+thaleslnr@users.noreply.github.com>
2018-11-26 15:10:46 -0200

    Update install.sh

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,4 +1,4 @@
 cp /var/www/html/urt/codedeploy/zz_malutrom.conf /etc/nginx/sites-available/
-ls -s /etc/nginx/sites-available/zz_malutrom.conf /etc/nginx/sites-enabled/zz_malutrom.conf
+ln -s /etc/nginx/sites-available/zz_malutrom.conf /etc/nginx/sites-enabled/zz_malutrom.conf
 service nginx restart
 chmod -R 777 /var/www
