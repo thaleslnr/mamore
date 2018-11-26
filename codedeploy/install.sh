7c6f615f2f34fcc79d928cf8192a2779a7f0ab49
thaleslnr <44848784+thaleslnr@users.noreply.github.com>
2018-11-26 15:18:32 -0200

    Update install.sh

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,4 +1,4 @@
-cp /var/www/html/urt/codedeploy/zz_mamore.conf /etc/nginx/sites-available/
+cp /var/www/html/mamore/codedeploy/zz_mamore.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/zz_mamore.conf /etc/nginx/sites-enabled/zz_malutrom.conf
 service nginx restart
 chmod -R 777 /var/www
